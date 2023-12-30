fdc43dc6e551bfcb01726a3f382c174440737a76
Pranav <pranavshelke4@gmail.com>
2023-12-30 17:09:31 +0530

    Refactor QuickContact component and styles
    This commit refactors the QuickContact component by removing an unused SVG element and updates the QuickContact.css file to simplify the border-radius property. Additionally, it adjusts the margin-left property in ServicesInfo.css and updates the gap property in WideRange.css to improve responsiveness.

--- src/Component/Services/QuickContact.jsx
+++ src/Component/Services/QuickContact.jsx
@@ -14,25 +14,6 @@ function QuickContact() {
             ENQUIRE NOW
           </NavLink>
         </button>
-        <div className="QuickContact-btn-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-arrow-narrow-right"
-            width="35"
-            height="35"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#2c3e50"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M5 12l14 0" />
-            <path d="M15 16l4 -4" />
-            <path d="M15 8l4 4" />
-          </svg>
-        </div>
       </div>
     </div>
   );
