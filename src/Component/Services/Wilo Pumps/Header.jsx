3348d45be7717efbadb5c248144a44235003a7ea
Pranav <pranavshelke4@gmail.com>
2024-01-05 18:57:38 +0530

    Refactor Wilo Pumps Header component and CSS
    This commit refactors the Wilo Pumps Header component and its associated CSS file. Changes include fixing import statements, updating button styles, and adding new buttons for contact and learning more.

--- src/Component/Services/Wilo Pumps/Header.jsx
+++ src/Component/Services/Wilo Pumps/Header.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "../../../Style/Services/Header.css";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
 
 function Header() {
   return (
     <div className="Willoheader">
       <h1 className="header-title">
-      Authorized Channel <br /> Partner for <span>wilo</span>    
+        Authorized Channel <br /> Partner for <span>wilo</span>
       </h1>
-      <button className="header-button">
-        <HashLink to={"#products"} className="know_more_btn">Know More</HashLink >
-      </button>
+
+      <div className="willo-header-button">
+        <button className="header-button">
+          <HashLink to={"/contact-us"} className="know_more_btn">
+            ENQUIRE NOW
+          </HashLink>
+        </button>
+        <button className="header-button">
+          <HashLink to={"#products"} className="know_more_btn">
+            LEARN MORE
+          </HashLink>
+        </button>
+      </div>
     </div>
   );
 }
