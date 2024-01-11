bf31cd0e74d79f617d3a3cf1ffda5f01da14d39e
Pranav <pranavshelke4@gmail.com>
2024-01-11 15:22:15 +0530

    Update styling and content in SecendSection component

--- src/Component/Services/Fire Fighting Services/SecendSection.jsx
+++ src/Component/Services/Fire Fighting Services/SecendSection.jsx
@@ -4,7 +4,7 @@ import "../../../Style/Services/SecendSection.css";
 function SecendSection() {
   return (
     <div className="SecendSection">
-      <h1><span className="blue-sub-title">GOVERNMENT-APPROVED LICENSED</span> FIREFIGHTING CONTRACTOR </h1>
+      <h1><span className="blue-sub-title">GOVERNMENT-APPROVED <br /> LICENSED</span> FIREFIGHTING <br /> CONTRACTOR </h1>
     </div>
   );
 }
